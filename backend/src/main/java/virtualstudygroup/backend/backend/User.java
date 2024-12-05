cfe80ba19e6bc927444dab54090e1629959e100c
lmiholic <lukas.miholiclol@gmail.com>
2024-12-05 13:15:41 +0100

    database models generated

---
+++ backend/src/main/java/virtualstudygroup/backend/backend/User.java
@@ -0,0 +1,53 @@
+package virtualstudygroup.backend.backend;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @ColumnDefault("nextval('users_id_seq')")
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Size(max = 255)
+    @NotNull
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Size(max = 255)
+    @NotNull
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Size(max = 255)
+    @NotNull
+    @Column(name = "password_hash", nullable = false)
+    private String passwordHash;
+
+    @Column(name = "profile_picture", length = Integer.MAX_VALUE)
+    private String profilePicture;
+
+    @ColumnDefault("CURRENT_TIMESTAMP")
+    @Column(name = "created_at")
+    private Instant createdAt;
+
+    @ColumnDefault("CURRENT_TIMESTAMP")
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @ColumnDefault("1")
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+}
