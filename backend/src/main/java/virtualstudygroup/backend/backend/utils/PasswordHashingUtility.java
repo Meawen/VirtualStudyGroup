c79e714c37aff7342389875e8350fe39d0dd5253
lmiholic <lukas.miholiclol@gmail.com>
2025-01-20 15:14:59 +0100

    login and register fully work

--- backend/src/main/java/virtualstudygroup/backend/backend/utils/PasswordHashingUtility.java
+++ backend/src/main/java/virtualstudygroup/backend/backend/utils/PasswordHashingUtility.java
@@ -2,7 +2,9 @@ package virtualstudygroup.backend.backend.utils;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
 
+@Component
 public class PasswordHashingUtility {
     private static final PasswordEncoder encoder = new BCryptPasswordEncoder();
     public static String hashPassword(String password) {
